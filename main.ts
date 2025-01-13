9af66e1179a33065f5776ae87f1ed50e8aed25e6
Ali-Elouasbi <ali.elouasbi@mths.ca>
2025-01-13 10:25:09 -0500

    changed code

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Ali Elouasbi
+ * Created on: Jan 2025
+ * This program takes a picture on your phone using a microbit
 */
 
-basic.showString('Hello, World!')
+//setup
+basic.showIcon(IconNames.Happy)
+basic.clearScreen()
+
+//When Button A is pressed
+input.onButtonPressed(Button.A, function () {
+    devices.raiseAlertTo(MesAlertEvent.DisplayToast)
+    devices.tellCameraTo(MesCameraEvent.TakePhoto)
+})
+
+//When Button B is pressed
+input.onButtonPressed(Button.B, function () {
+    devices.tellCameraTo(MesCameraEvent.ToggleFrontRear)
+})
+basic.showString("A = photo B = Front")
